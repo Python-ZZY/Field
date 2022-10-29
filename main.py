cd9b0ae0bd7bbc74a1eb54e9bb6e1782717f9255
Python-ZZY <98807228+Python-ZZY@users.noreply.github.com>
2022-10-29 19:15:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ except:
     pass
 
 import pygame as pg
+import asyncio
 from configs import WIDTH, HEIGHT
 from scenes import Menu
 
-if __name__ == "__main__":
+async def main():
     pg.init()
 
     pg.display.set_mode((WIDTH, HEIGHT))
 
     Menu()
+
+if __name__ == "__main__":
+    asyncio.run(main())
